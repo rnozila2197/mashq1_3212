7c8b22152a3aab4f4c5a4dff0d135c15eba70a26
rnozila2197 <yjamshid2197@gmail.com>
2025-12-01 15:33:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1
+matn = "mathematica"
+print(matn[0:4])
+
+# 2
+matn = "development"
+print(matn[0:7])
+
+# 3
+matn = "programingLanguage"
+print(matn[-8:-1])
+
+# 4
+matn = "abcdefghijklm"
+print(slice(matn))
+
+# 5
+son = "0123456789"
+print(len(son[0:10:3]))
